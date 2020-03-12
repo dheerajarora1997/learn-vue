7b764822977cb290a54af0b65b63197124a4ffa7
Dheeraj Arora <dheeraj.arora1997@yahoo.com>
2020-03-12 22:03:02 +0530

    game file added

---
+++ app.js
@@ -0,0 +1,81 @@
+new Vue({
+    el: '#app',
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        onGame: false,
+        turns: [
+
+        ]
+    },
+    methods: {
+        startGame: function () {
+            this.onGame = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.turns = [];
+        },
+        attack: function () {
+            var damage = this.calculateDamage(3, 10)
+            this.monsterHealth -= damage;
+
+            if (this.checkWin()) {
+                return;
+            }
+            this.monsterAttack();
+
+        },
+        heal: function () {
+            if (this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+
+            this.monsterAttack();
+
+        },
+        splAttack: function () {
+            damage = this.calculateDamage(10, 15);
+            this.monsterHealth -= damage;
+
+            if (this.checkWin()) {
+                return;
+            }
+            this.monsterAttack();
+
+        },
+        quit: function () {
+            this.onGame = false;
+        },
+        monsterAttack: function () {
+            damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+
+            this.checkWin();
+        },
+        calculateDamage: function (min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+
+        },
+        checkWin: function () {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You win! New Game')) {
+                    this.startGame();
+                } else {
+                    this.onGame = false;
+                }
+                this.onGame = false;
+                return true;
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You Lost! Start Game')) {
+                    this.startGame();
+                } else {
+                    this.onGame = false;
+                }
+                return false;
+            }
+
+        }
+    }
+});
